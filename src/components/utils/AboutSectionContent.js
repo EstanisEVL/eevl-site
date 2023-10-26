6f6b57791f8b7e7de7d000620c5cc63f1d163ed6
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2023-10-26 14:11:05 -0300

    fix: tech list updated.

--- src/components/utils/AboutSectionContent.js
+++ src/components/utils/AboutSectionContent.js
@@ -1,9 +1,4 @@
 export const AboutSectionContent = {
-  firstP:
-    "Hello! My name is Estanislao and I enjoy building web applications. My interest in web development started back in 2015 when I decided to try building a custom web site for my parent's business — turns out trying to modify different themes and plugins left me with a huge need to learn more about this world!",
-  secondP:
-    "Currently, I devote myself to the wonderful process of becoming a Full-stack Developer. I understand that this process never truly ends, and that's what I love about it! So far I have WordPress, HTML, CSS, JavaScript, Bootstrap, Sass, Git/GitHub and React JS under my belt. I want to keep expanding that belt, adding Typescript, Node.JS, Express.js, MySQL and MongoDB. I'm an extremely curious person, and I devote myself to learn any skill that will help turn my ideas into code!",
-  ThirdP: "Here are a few technologies I've been working with recently:",
   skills: [
     { id: 1, title: "HTML & CSS" },
     { id: 2, title: "JavaScript (ES6+)" },
@@ -11,5 +6,9 @@ export const AboutSectionContent = {
     { id: 4, title: "SASS" },
     { id: 5, title: "Bootstrap" },
     { id: 6, title: "WordPress" },
+    { id: 6, title: "Styled-components" },
+    { id: 7, title: "Node Js" },
+    { id: 8, title: "Express" },
+    { id: 9, title: "MongoDb" },
   ],
 };
